4abd113328e5b3dcf6a63f8ef5f3a3a33e15144f
Mahmudul Hasan <jony@Mahmuduls-MacBook-Pro.local>
2019-03-19 20:38:35 +0200

    react route

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+
+
+
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          
+            <Switch>
+              <Route path="/" component={ Home } exact/>
+              <Route path="/about" component={ About } />
+              <Route path="/contact" component={ Contact } />
+              <Route component={ Error } />
+            </Switch>
+          
+        </BrowserRouter>
+
+
+    );
+  }
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () =>{
+	return(
+			<div>
+				<p>This is About</p>				
+			</div>
+		);
+}
+
+export default About

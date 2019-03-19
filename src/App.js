db80e53937ddd55dd07f6bcdde43917029005b2d
Mahmudul Hasan <jony@Mahmuduls-MacBook-Pro.local>
2019-03-19 22:47:01 +0200

    Navigation has been added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Navigation from './components/Navigation';
 import Error from './components/Error';
 
 
@@ -15,7 +16,7 @@ class App extends Component {
   render() {
     return (
         <BrowserRouter>
-          
+            <Navigation />
             <Switch>
               <Route path="/" component={ Home } exact/>
               <Route path="/about" component={ About } />
